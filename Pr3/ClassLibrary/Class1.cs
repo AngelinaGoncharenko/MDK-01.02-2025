74fdc2eacbd4d4a6e4f42dc8001c2b607a2663d3
Ангелина Гончаренко <angelinagoncharenko5@gmail.com>
2025-02-09 18:06:31 +0500

    практика 1 от лекции 3

---
+++ Pr3/ClassLibrary/Class1.cs
@@ -0,0 +1,43 @@
+﻿namespace ClassLibrary
+{
+    public class Class1
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль невозможно!");
+            }
+            return a / b;
+        }
+
+        public static double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public static double Modulo(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Деление на ноль невозможно!");
+            }
+            return a % b;
+        }
+    }
+}
--- Pr3/Pr3/Program.cs
+++ Pr3/Pr3/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Pr3
+﻿using ClassLibrary;
+using System;
+
+namespace Pr3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Число a:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Число b:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double sum = Class1.Add(a, b);
+            Console.WriteLine($"Сумма: {sum}");
+
+            double difference = Class1.Subtract(a, b);
+            Console.WriteLine($"Разность: {difference}");
+
+            double product = Class1.Multiply(a, b);
+            Console.WriteLine($"Произведение: {product}");
+
+            double quotient = Class1.Divide(a, b);
+            Console.WriteLine($"Частное: {quotient}");
+
+            double power = Class1.Power(a, b);
+            Console.WriteLine($"a в степени b: {power}");
+
+            double modulo = Class1.Modulo(a, b);
+            Console.WriteLine($"Остаток от деления a на b: {modulo}");
         }
     }
 }
